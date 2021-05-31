3ae6c00cf35a6f2f7061719704ec90dc8a5a90ef
Marc Sevilleja <marc.sevilleja@gmail.com>
2021-05-30 22:40:22 -0700

    Created code to save/load through XML Serialization

--- Assets/Scripts/Core/SaveManager.cs
+++ Assets/Scripts/Core/SaveManager.cs
@@ -8,6 +8,7 @@ using System.Xml.Serialization;
 public class SaveManager : MonoBehaviour
 {
     public SaveData activeSave;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +20,47 @@ public class SaveManager : MonoBehaviour
     {
         
     }
+
+    public void Save()
+    {
+        string dataPath = Application.persistentDataPath;
+
+        var serializer = new XmlSerializer(typeof(SaveData));
+
+        // Location of where to create/save the fileStream
+        var stream = new FileStream(dataPath + "/" + activeSave.saveName + ".save", FileMode.Create);
+
+        // Serialize the actual object to save (SaveData using its saveName)
+        serializer.Serialize(stream, activeSave);
+        stream.Close();
+
+        Debug.Log("save: " + activeSave.saveName + " created!");
+    }
+
+    public void Load()
+    {
+        string dataPath = Application.persistentDataPath;
+
+        if (System.IO.File.Exists(dataPath + "/" + activeSave.saveName + ".save"))
+        {
+            var serializer = new XmlSerializer(typeof(SaveData));
+
+            // Location of where to open the save
+            var stream = new FileStream(dataPath + "/" + activeSave.saveName + ".save", FileMode.Create);
+            activeSave = serializer.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            Debug.Log("Save: " + activeSave.saveName + " loaded!");
+        }
+    }
 }
 
 [System.Serializable]
 public class SaveData
 {
+    // denotes which save the player wants to use
+    public string saveName;
+
     // player level and stats
     //public int playerLevel;       // may not need, just add all levels together to get player level
     public int playerVitalityLevel;
